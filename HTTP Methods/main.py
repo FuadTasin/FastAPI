2d7d091632fb17d3ce01a90dec65a6e1537714f4
Fuad Tasin <tasinhasan183618@gmail.com>
2026-08-29 22:22:49 +0600

    Added the get request part

---
+++ HTTP Methods/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI,Path,HTTPException
+import json
+
+app=FastAPI()
+
+def load_data():
+    with open('students.json','r') as f:
+        data=json.load(f)
+    return data
+
+@app.get("/")
+def hello():
+    return "Student Management System App."
+
+@app.get("/about")
+def about():
+    return "A fully functional API to manage our student records."
+
+@app.get("/view")
+def view_students():
+    data=load_data()
+    return data
