e8d7d353c540bf03678d4ada2bbaa307f5273b65
Fuad Tasin <tasinhasan183618@gmail.com>
2026-09-04 23:49:43 +0600

    Added endpoints to to get the info of the database(all) and to view a specific data.

--- Database With SQLAlchemy/main.py
+++ Database With SQLAlchemy/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends, HTTPException
 import tables
-from database import engine
+from tables import Todos
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import engine,SessionLocal
 
 app=FastAPI()
 
 tables.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency=Annotated[Session,Depends(get_db)]
+
+@app.get('/')
+def read_todos(db:db_dependency):
+    return db.query(Todos).all()
+
+@app.get('/todos/{todo_id}')
+def read_specific_todos(db:db_dependency,todo_id:int):
+    specific_todo=db.query(Todos).filter(Todos.id==todo_id).first()
+
+    if (specific_todo):
+        return specific_todo
+    else:
+        raise HTTPException(status_code=404,detail='Todo not found.')
