ba4b3c1b80b837e38faa58d73efa13c0c87955c0
Fuad Tasin <tasinhasan183618@gmail.com>
2026-09-05 20:11:23 +0600

    Added a endpoint to create the todo(with pydantic validation and without giving the id,it will auto update each time).

--- Database With SQLAlchemy/main.py
+++ Database With SQLAlchemy/main.py
@@ -4,8 +4,16 @@ from tables import Todos
 from typing import Annotated
 from sqlalchemy.orm import Session
 from database import engine,SessionLocal
+from pydantic import BaseModel, Field
+from fastapi.responses import JSONResponse
 
 app=FastAPI()
+
+class Todo(BaseModel):
+    title:Annotated[str,""]
+    description:Annotated[str,""]
+    priority:Annotated[int,Field(gt=0,le=5,description="")]
+    completed:Annotated[bool,Field(default=False)]
 
 tables.Base.metadata.create_all(bind=engine)
 
@@ -30,3 +38,10 @@ def read_specific_todos(db:db_dependency,todo_id:int):
         return specific_todo
     else:
         raise HTTPException(status_code=404,detail='Todo not found.')
+
+@app.post('/create')
+def create_todos(db:db_dependency,new_todo:Todo):
+    todo_model=Todos(**new_todo.model_dump())
+    db.add(todo_model)
+    db.commit()
+    return JSONResponse(status_code=201,content={"message":"Todo Created Successfully"})
