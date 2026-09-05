53f4dcd3081f512c7daa5c802f88a1def31028d3
Fuad Tasin <tasinhasan183618@gmail.com>
2026-09-05 22:16:53 +0600

    Added endpoints to update a specific todo and delete a todo.

--- Database With SQLAlchemy/main.py
+++ Database With SQLAlchemy/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Depends, HTTPException
 import tables
 from tables import Todos
-from typing import Annotated
+from typing import Annotated,Optional
 from sqlalchemy.orm import Session
 from database import engine,SessionLocal
 from pydantic import BaseModel, Field
@@ -14,6 +14,12 @@ class Todo(BaseModel):
     description:Annotated[str,""]
     priority:Annotated[int,Field(gt=0,le=5,description="")]
     completed:Annotated[bool,Field(default=False)]
+
+class UpdateTodo(BaseModel):
+    title:Optional[str]=Field(default=None)
+    description:Optional[str]=Field(default=None)
+    priority:Optional[int]=Field(gt=0,lt=6,default=None)
+    completed:Optional[bool]=Field(default=None)
 
 tables.Base.metadata.create_all(bind=engine)
 
@@ -45,3 +51,28 @@ def create_todos(db:db_dependency,new_todo:Todo):
     db.add(todo_model)
     db.commit()
     return JSONResponse(status_code=201,content={"message":"Todo Created Successfully"})
+
+@app.put('/update/{todo_id}')
+def update_todos(db:db_dependency,todo_id:int,update_todo:UpdateTodo):
+    todo=db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo is  None:
+        raise HTTPException(status_code=404,detail="Todo not found.")
+    
+    update_data=update_todo.model_dump(exclude_unset=True)
+    for key,value in update_data.items():
+        setattr(todo,key,value)
+
+    db.commit()
+
+    return JSONResponse(status_code=200,content={'message':'Todo updated successfully.'})
+
+@app.delete('/remove/{todo_id}')
+def delete_todos(db:db_dependency,todo_id:int):
+    todo=db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo is  None:
+        raise HTTPException(status_code=404,detail="Todo not found.")
+    
+    db.query(Todos).filter(Todos.id==todo_id).delete()
+    db.commit()
+
+    return JSONResponse(status_code=200,content={'message':'Todo deleted successfully.'}) 
