43e9ecd3f03487d133f1a5f07c9e1ac7fa5d5890
Fuad Tasin <tasinhasan183618@gmail.com>
2026-09-15 23:25:56 +0600

    Added jwt encode and decode for postgres.

--- Database Postgres/router/auth.py
+++ Database Postgres/router/auth.py
@@ -1,19 +1,23 @@
-from email import message
+from ctypes import alignment
 from typing import Annotated
-from warnings import deprecated
-from fastapi import FastAPI,APIRouter,Depends
-import fastapi
+from fastapi import APIRouter,Depends, HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from pydantic_schemas import CreateUser
 from models import Users
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
+from jose import jwt
+from datetime import timedelta,datetime,timezone
 
 router=APIRouter()
 
 bcrypt_context=CryptContext(schemes=['bcrypt'],deprecated='auto')
+OAuth2_bearer=OAuth2PasswordBearer(tokenUrl='token')
+
+SECRET_KEY="127cf0676824de422291e1ea30ef45abb1015d7dec46ca7cc3eba952e830e337"
+ALGORITHM="HS256"
 
 def get_db():
     db=SessionLocal()
@@ -29,8 +33,27 @@ def authenticate_user(username,password,db):
     if user is None:
         return False
     if bcrypt_context.verify(password,user.hash_password):
-        return True
+        return user
     return False
+
+def create_user_token(username:str,user_id:int,expires_delta:timedelta):
+    encode={'sub':username,'id':user_id}
+    expires=datetime.now(timezone.utc)+expires_delta
+    encode.update({'exp':expires})
+    return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
+
+def get_current_user(token:Annotated[str,Depends(OAuth2_bearer)]):
+    try:
+        payload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username:str=payload.get('sub')
+        user_id:str=payload.get('id')
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=404,detail="User not found")
+        return {'username':username,'id':user_id}
+    except:
+        raise HTTPException(status_code=404,detail="User not found")
+
 
 @router.post('/create/users')
 def create_users(db:db_dependency,new_user:CreateUser):
@@ -50,7 +73,7 @@ def create_users(db:db_dependency,new_user:CreateUser):
 @router.post('/login/users')
 def login_users(db:db_dependency,user_form:Annotated[OAuth2PasswordRequestForm,Depends()]):
     user=authenticate_user(user_form.username,user_form.password,db)
-    if user:
-        return "User Authenticated."
-    else:
-        return "Failed Authentication."
+    if not user:
+        return "Failed Authentication"
+    token=create_user_token(user.username,user.id,timedelta(minutes=30))
+    return token
